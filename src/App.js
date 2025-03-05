d34f2cce838e37d4adbae775c916925ae7e6daf5
Sean Selby <seanselbyy@gmail.com>
2025-03-05 14:21:45 +1300

    Push changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Seems to work</p>
+        <p>Still working</p>
         <a
           className="App-link"
           href="https://reactjs.org"
